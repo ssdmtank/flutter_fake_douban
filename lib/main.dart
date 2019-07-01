70f176e1394848c737fa750e1576ccf2625d01d3
ssdmtank <598512076@qq.com>
2019-07-01 23:21:25 +0800

    首页

--- lib/main.dart
+++ lib/main.dart
@@ -16,6 +16,53 @@ void main() {
 class MyApp extends StatelessWidget {
   @override
   Widget build(BuildContext context) {
-    return null;
+    return RestartWidget(
+      child: MaterialApp(
+        theme: ThemeData(backgroundColor: Colors.white),
+        home: Scaffold(
+          //控制界面内容 body 是否重新布局来避免底部被覆盖了，比如当键盘显示的时候，重新布局避免被键盘盖住内容。默认值为 true
+          resizeToAvoidBottomPadding: false,
+          body: Container(
+            child: Text("123"),
+          ),
+        ),
+      ),
+    );
+  }
+}
+
+//这个组件用来重新加载整个child Widget的。当我们需要重启APP的时候，可以使用这个方案
+class RestartWidget extends StatefulWidget {
+  final Widget child;
+
+  RestartWidget({Key key, this.child})
+      : assert(child != null),
+        super(key: key);
+
+  static restartApp(BuildContext context) {
+    final _RestartWidgetState state =
+        context.ancestorStateOfType(const TypeMatcher<_RestartWidgetState>());
+    state.restartApp();
+  }
+
+  @override
+  _RestartWidgetState createState() => _RestartWidgetState();
+}
+
+class _RestartWidgetState extends State<RestartWidget> {
+  Key key = UniqueKey();
+
+  void restartApp() {
+    setState(() {
+      UniqueKey key = UniqueKey();
+    });
+  }
+
+  @override
+  Widget build(BuildContext context) {
+    return Container(
+      key: key,
+      child: widget.child,
+    );
   }
 }
---
+++ lib/pages/splash/splash_widget.dart
@@ -0,0 +1,14 @@
+import 'package:flutter/material.dart';
+
+//App 首页
+class SplashWidget extends StatefulWidget {
+  @override
+  _SplashWidgetState createState() => _SplashWidgetState();
+}
+
+class _SplashWidgetState extends State<SplashWidget> {
+  @override
+  Widget build(BuildContext context) {
+    return null;
+  }
+}
